161b932fcfbd042717c257089f99b66e291fc035
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-26 16:48:35 -0300

    Add Person class to animal.rb setters/getters

---
+++ animal.rb
@@ -0,0 +1,10 @@
+class Person
+  def initialize(name, age, parent_permission)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+end
