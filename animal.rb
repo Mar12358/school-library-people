89eab2814dcb3bf3a3dcc37c0f2673e79afe19bc
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-26 16:55:46 -0300

    Add of_age? and can_use_services? methods

--- animal.rb
+++ animal.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(name, age, parent_permission)
+  def initialize(name, age, _parent_permission)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -7,4 +7,16 @@ class Person
 
   attr_reader :id
   attr_accessor :name, :age
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
 end
