8655f544e45d98e01e8e4ad17db0fba718da65db
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-28 14:04:33 -0300

    Create relationship between Classroom and Student

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
   def initialize(label = 'Unknown')
     @label = label
+    @students = [] # Initialize an empty array to store students
   end
+
   attr_accessor :label
+  attr_reader :students # Add a reader method to access the students array
+
+  def add_student(student)
+    @students << student
+    student.classroom = self # Set the classroom for the student
+  end
 end
--- student.rb
+++ student.rb
@@ -4,7 +4,10 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super('Student', age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
+
+  attr_accessor :classroom
 
   def play_hockey
     '¯(ツ)/¯'
