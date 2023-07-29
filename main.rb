ad7d2e5408eeb304355219dfb7fbf59abc536d0d
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-29 14:17:09 -0300

    Fix Linters (too many lines for block)

--- main.rb
+++ main.rb
@@ -24,38 +24,16 @@ loop do
   when '2'
     app.list_people
   when '3'
-    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]'
-    user_input3 = gets.chomp
-
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    if user_input3 == '1'
-      print 'Has parent permission? [Y/N]: '
-      permission = gets.chomp
-      permission = permission.downcase == 'y'
-      app.create_student(age, name, permission)
-    elsif user_input3 == '2'
-      print 'Specialization: '
-      specialization = gets.chomp
-      app.create_teacher(age, name, specialization)
-    else
-      puts 'Invalid Option'
-    end
+    app.create_person
   when '4'
-    print 'Book Title: '
-    title = gets.chomp
-    print 'Book Author: '
-    author = gets.chomp
-    app.create_book(title, author)
+    app.create_book
+  when '5'
+    app.create_rental
   when '6'
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    app.list_rentals(person_id)
+    app.list_rentals
   when '7'
     puts 'Bye bye'
-      break
+    break
   else
 
     puts 'Invalid input. Please choose a valid option (1 to 7).'
